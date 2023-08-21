e0ea265bdd49c3a741bfca43e1ad28bb5b7fa700
Tatiana Kashcha <tkashcha@gmail.com>
2023-08-21 13:30:24 +0300

    Update contacts.js

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -24,35 +24,6 @@ const getContact = async (req, res, next) => {
   }
 };
 
-// const addNewContact = async (req, res, next) => {
-//   try {
-//     const { error } = schemas.addSchema.validate(req.body);
-//     const emptyBody = Object.keys(req.body).length === 0;
-//     const missingField =
-//       schemas.addSchema._ids._byKey.size !== Object.keys(req.body).length;
-
-//     if (emptyBody) {
-//       throw HttpError(400, "missing fields");
-//     }
-
-//     if (error) {
-//       if (missingField) {
-//         throw HttpError(
-//           400,
-//           `missing required ${error.details[0].path[0]} field`
-//         );
-//       } else {
-//         throw HttpError(400, error.message);
-//       }
-//     }
-
-//     const result = await contacts.addContact(req.body);
-//     res.status(201).json(result);
-//   } catch (error) {
-//     next(error);
-//   }
-// };
-
 const addNewContact = async (req, res, next) => {
   try {
     const result = await contacts.addContact(req.body);
@@ -76,39 +47,6 @@ const delContact = async (req, res, next) => {
     next(error);
   }
 };
-
-// const editContact = async (req, res, next) => {
-//   try {
-//     const { error } = schemas.addSchema.validate(req.body);
-//     const emptyBody = Object.keys(req.body).length === 0;
-//     const missingField =
-//       schemas.addSchema._ids._byKey.size !== Object.keys(req.body).length;
-
-//     if (emptyBody) {
-//       throw HttpError(400, "missing fields");
-//     }
-
-//     if (error) {
-//       if (missingField) {
-//         throw HttpError(
-//           400,
-//           `missing required ${error.details[0].path[0]} field`
-//         );
-//       } else {
-//         throw HttpError(400, error.message);
-//       }
-//     }
-
-//     const { contactId } = req.params;
-//     const result = await contacts.updateContact(contactId, req.body);
-//     if (!result) {
-//       throw HttpError(404, "Not found");
-//     }
-//     res.json(result);
-//   } catch (error) {
-//     next(error);
-//   }
-// };
 
 const editContact = async (req, res, next) => {
   try {
