6167056f391aa07c947951806a8e3bebc8e496dc
Tatiana Kashcha <tkashcha@gmail.com>
2023-08-23 23:20:58 +0300

    upd messages

--- schemas/contacts.js
+++ schemas/contacts.js
@@ -1,10 +1,25 @@
 const Joi = require("joi");
 
 const addSchema = Joi.object({
-  name: Joi.string().required(),
-  email: Joi.string().required(),
-  phone: Joi.string().required(),
+  name: Joi.string().required().messages({
+    "any.required": "missing required name field",
+  }),
+  email: Joi.string().required().messages({
+    "any.required": "missing required email field",
+  }),
+  phone: Joi.string().required().messages({
+    "any.required": "missing required phone field",
+  }),
 });
+
+// const customMessages = {
+//   'any.required': 'missing required {#label} field'
+// };
+// const addSchema = Joi.object({
+//   name: Joi.string().required().messages(customMessages),
+//   email: Joi.string().email().required().messages(customMessages),
+//   phone: Joi.string().required().messages(customMessages)
+// });
 
 module.exports = {
   addSchema,
