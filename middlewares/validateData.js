70f2464b3791d86d770cb235d5491cb5bb59ae2c
Tatiana Kashcha <tkashcha@gmail.com>
2023-08-23 23:37:16 +0300

    upd mess

--- middlewares/validateData.js
+++ middlewares/validateData.js
@@ -4,8 +4,6 @@ const validateData = (schema) => {
   const func = (req, res, next) => {
     const { error } = schema.validate(req.body);
     const emptyBody = Object.keys(req.body).length === 0;
-    // const missingField =
-    //   schema._ids._byKey.size !== Object.keys(req.body).length;
 
     if (emptyBody) {
       next(HttpError(400, "missing fields"));
@@ -13,13 +11,6 @@ const validateData = (schema) => {
 
     if (error) {
       next(HttpError(400, error.message));
-      // if (missingField) {
-      //   next(
-      //     HttpError(400, `missing required ${error.details[0].path[0]} field`)
-      //   );
-      // } else {
-      //   next(HttpError(400, error.message));
-      // }
     }
     next();
   };
--- schemas/contacts.js
+++ schemas/contacts.js
@@ -12,15 +12,6 @@ const addSchema = Joi.object({
   }),
 });
 
-// const customMessages = {
-//   'any.required': 'missing required {#label} field'
-// };
-// const addSchema = Joi.object({
-//   name: Joi.string().required().messages(customMessages),
-//   email: Joi.string().email().required().messages(customMessages),
-//   phone: Joi.string().required().messages(customMessages)
-// });
-
 module.exports = {
   addSchema,
 };
