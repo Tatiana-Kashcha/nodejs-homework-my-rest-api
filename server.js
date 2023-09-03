bef4b3ebdd09380b4cdf4ea8631fc5edc2194e43
Tatiana Kashcha <tkashcha@gmail.com>
2023-09-03 19:23:53 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const app = require("./app");
 const mongoose = require("mongoose");
 
 const { DB_HOST, PORT = 3000 } = process.env;
+console.log(process.env.DB_HOST);
 
 mongoose.set("strictQuery", true);
 
