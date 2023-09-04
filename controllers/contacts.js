68a184cf98251927e27192d8837bf1cd2d3cc6e3
Tatiana Kashcha <tkashcha@gmail.com>
2023-09-04 10:26:19 +0300

    upd contacts

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -24,8 +24,9 @@ const getContactById = async (req, res, next) => {
 };
 
 const addContact = async (req, res, next) => {
+  const { _id: owner } = req.user;
   try {
-    const result = await Contact.create(req.body);
+    const result = await Contact.create({ ...req.body, owner });
     res.status(201).json(result);
   } catch (error) {
     next(error);
