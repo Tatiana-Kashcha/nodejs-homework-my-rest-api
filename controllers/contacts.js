18684318b57aaefa4e70a6e34e1203695e64d279
Tatiana Kashcha <tkashcha@gmail.com>
2023-09-07 02:32:10 +0300

    Update contacts.js

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -3,13 +3,10 @@ const { HttpError } = require("../helpers");
 
 const getAllContacts = async (req, res, next) => {
   const { _id: owner } = req.user;
-  const { page = 1, limit = 20 } = req.query;
-  const skip = (page - 1) * limit;
+
   try {
-    const result = await Contact.find({ owner }, { skip, limit }).populate(
-      "owner",
-      "email subscription"
-    );
+    const result = await Contact.find({ owner });
+
     res.json(result);
   } catch (error) {
     next(error);
