377e75b5192839b3857fca2815ceba015f1ec223
Tatiana Kashcha <tkashcha@gmail.com>
2023-09-08 19:05:58 +0300

    upd token

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -17,12 +17,9 @@ const authenticate = async (req, res, next) => {
     const { id } = jwt.verify(token, SECRET_KEY);
     const user = await User.findById(id);
 
-    if (!user) {
+    if (!user || !user.token || user.token !== token) {
       next(HttpError(401, "Not authorized"));
     }
-    // if (!user || !user.token || user.token !== token) {
-    //   next(HttpError(401, "Not authorized"));
-    // }
     req.user = user;
     next();
   } catch {
