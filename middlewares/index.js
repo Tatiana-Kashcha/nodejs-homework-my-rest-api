64d1cb33a2e8f3d95d83c3c3e378b7f9f635500e
Tatiana Kashcha <tkashcha@gmail.com>
2023-09-10 01:59:57 +0300

    add upload

--- middlewares/index.js
+++ middlewares/index.js
@@ -2,10 +2,12 @@ const validateData = require("./validateData");
 const isValidId = require("./isValidId");
 const validateStatus = require("./validateStatus");
 const authenticate = require("./authenticate");
+const upload = require("./upload");
 
 module.exports = {
   validateData,
   isValidId,
   validateStatus,
   authenticate,
+  upload,
 };
--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const app = require("./app");
 const mongoose = require("mongoose");
 
 const { DB_HOST, PORT = 3000 } = process.env;
-// console.log(process.env.DB_HOST);
 
 mongoose.set("strictQuery", true);
 
